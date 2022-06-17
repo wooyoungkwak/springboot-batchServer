864a10c456b6049597363460eb91492cc3904907
zilet123456 <zilet123456@gmail.com>
2022-06-17 15:13:16 +0900

    1. ObjectMapper 의 설정 추가 ( date format & java Time module 추가 )

--- src/main/java/com/example/springbootbatchserver/configuration/UtilConfiguration.java
+++ src/main/java/com/example/springbootbatchserver/configuration/UtilConfiguration.java
@@ -1,7 +1,11 @@
 package com.example.springbootbatchserver.configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 /**
  * Date : 2022-06-17
@@ -14,8 +18,10 @@ public class UtilConfiguration {
     @Bean
     public ObjectMapper objectMapper(){
 
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
         ObjectMapper objectMapper = new ObjectMapper();
-
+        objectMapper.setDateFormat(dateFormat);
+        objectMapper.registerModule(new JavaTimeModule());  // LocalDateTime 변환을위한 모듈 등록 (필수)
         return objectMapper;
     }
 }
